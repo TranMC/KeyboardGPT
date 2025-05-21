18ab58dae8eda8981b5250ee727b435de480f453
TranMC <tranmc2005@gmail.com>
2025-05-21 15:51:19 +0700

    Sửa package

---
+++ app/src/main/java/tn/amin/keyboard_gpt/instruction/prefix/InstructionPrefixTreater.java
@@ -0,0 +1,28 @@
+package tn.amin.keyboard_gpt.instruction.prefix;
+
+import tn.amin.keyboard_gpt.SPManager;
+import tn.amin.keyboard_gpt.UiInteracter;
+import tn.amin.keyboard_gpt.instruction.TextTreater;
+
+public class InstructionPrefixTreater implements TextTreater {
+    private final SPManager mSPManager;
+    private final UiInteracter mInteracter;
+
+    public InstructionPrefixTreater(SPManager spManager, UiInteracter interacter) {
+        mSPManager = spManager;
+        mInteracter = interacter;
+    }
+
+    @Override
+    public boolean treat(String text) {
+        if (text == null || text.isEmpty()) {
+            showInstructionPrefixDialog();
+            return true;
+        }
+        return false;
+    }
+
+    private void showInstructionPrefixDialog() {
+        mInteracter.showInstructionPrefixDialog();
+    }
+} 
